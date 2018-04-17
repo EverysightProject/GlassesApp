4c6c5d1a743e001a154860e20265b8a92918669c
ygoddard <yuval.goddard@gmail.com>
2018-04-17 22:24:06 +0300

    presentation version

---
+++ samples/GlassesApp/src/main/java/Places/PlaceId.java
@@ -0,0 +1,8 @@
+package Places;
+
+/**
+ * Created by Yuval on 24/10/2017.
+ */
+
+public class PlaceId {
+}
---
+++ samples/GlassesApp/src/main/java/internals/StringJoin.java
@@ -0,0 +1,8 @@
+package internals;
+
+/**
+ * Created by Yuval on 31/12/2017.
+ */
+
+public class UrlValue {
+}
